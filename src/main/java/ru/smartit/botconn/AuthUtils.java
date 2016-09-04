8250e6f4439961b44f116fec90cbad1ec86735f2
Igor404 <bolshakov.kruv@gmail.com>
2016-09-04 16:30:42 +0500

    resources refactoring

--- src/main/java/ru/smartit/botconn/AuthUtils.java
+++ src/main/java/ru/smartit/botconn/AuthUtils.java
@@ -1,7 +1,6 @@
 package ru.smartit.botconn;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.istack.internal.Nullable;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,15 +9,18 @@ import java.io.InputStream;
  * Created by bolsh_000 on 9/4/2016.
  */
 public class AuthUtils {
-    public static @Nullable AuthData getAuthData() {
+    public static AuthData getAuthData() {
         AuthData res = null;
         try {
             ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-            InputStream is = classloader.getResourceAsStream("auth.properties");
+            InputStream is = classloader.getResourceAsStream("auth.json");
             ObjectMapper mapper = new ObjectMapper();
             res = mapper.readValue(is, AuthData.class);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        if (res == null) {
+            throw new Error("Error while reading auth.json file!");
         }
         return res;
     }
