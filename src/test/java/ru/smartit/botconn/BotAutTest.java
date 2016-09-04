9504db89b0d33d6c01313b9f3f0f7d3aa5575d0a
Igor404 <bolshakov.kruv@gmail.com>
2016-09-04 16:13:11 +0500

    resources added

--- src/test/java/ru/smartit/botconn/BotAutTest.java
+++ src/test/java/ru/smartit/botconn/BotAutTest.java
@@ -1,15 +1,29 @@
 package ru.smartit.botconn;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
+import ru.smartit.botconn.model.AuthData;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 public class BotAutTest extends Assert {
 
     @Test
     public void testGetToken() throws IOException {
 
+    }
+
+    @Test
+    public void testGetAuthData() throws IOException {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream("auth.properties");
+        ObjectMapper mapper = new ObjectMapper();
+        AuthData jsonMap = mapper.readValue(is, AuthData.class);
+        assertNotNull(jsonMap.CLIENT_ID);
+        assertNotNull(jsonMap.CLIENT_SECRET);
     }
 
 }
---
+++ src/test/java/ru/smartit/botconn/model/AuthData.java
@@ -0,0 +1,9 @@
+package ru.smartit.botconn.model;
+
+/**
+ * Created by bolsh_000 on 9/4/2016.
+ */
+public class AuthData {
+    public String CLIENT_ID;
+    public String CLIENT_SECRET;
+}
